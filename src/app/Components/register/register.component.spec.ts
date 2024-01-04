dd7ce6001fb8f400280cb3453d508903915cf00b
otaka00 <ahmad_otaca@hotmail.com>
2024-01-04 20:59:49 +0200

    Implemented register component

--- src/app/Components/register/register.component.spec.ts
+++ src/app/Components/register/register.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,10 +9,11 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      imports: [ReactiveFormsModule],
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+constructor(private fb: FormBuilder, private router: Router, private userService: UserService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      reenteredPassword: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+register() {
+if (this.registerForm.valid) {
+//  const email = this.registerForm.value.email;
+//
+//       // Check if the email is already taken
+//       if (this.userService.isEmailTaken(email)) {
+//         alert('Email is already taken. Please choose another one.');
+//       } else {
+//         // Add the new user
+//         this.userService.addUser({
+//           email: email,
+//           password: this.registerForm.value.password,
+//         });
+//
+//         // Navigate to login after successful registration
+//         this.router.navigate(['/login']);
+//         alert('Congrats! Account created successfully.');
+//       }
+//     } else {
+//       alert('Please enter all required fields.');
+//     }
+      const { email, password } = this.registerForm.value;
+      this.userService.saveUser(email, password);
+      this.router.navigate(['/login']);
+      alert('Congrats! Account created successfully.');
+ }
+    else alert('Please enter all required fields.');
+    console.log('Registration form submitted:', this.registerForm.value);
+  }
+  ngOnInit(): void {
+  }
 
 }
